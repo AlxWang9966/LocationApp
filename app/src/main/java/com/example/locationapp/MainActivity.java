ccc1bab85c9d322eed85a9f031797c1ce6ec7b96
AlxWang9966 <wycalex@bu.edu>
2022-11-08 13:52:13 -0500

    xml done

--- app/src/main/java/com/example/locationapp/MainActivity.java
+++ app/src/main/java/com/example/locationapp/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.locationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationServices;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+             implements GoogleApiClient.ConnectionCallbacks,
+                        GoogleApiClient.OnConnectionFailedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
